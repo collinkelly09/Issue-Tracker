46386ec6dd2a8c1f9873182525e908c0453b5ef3
Collin Kelly <collinkelly09@gmail.com>
2024-11-21 21:56:02 -0700

    delete issue api built

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -32,3 +32,21 @@ export async function PATCH(
 
     return NextResponse.json(updatedIssue);
 }
+
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    const { id } = await params;
+
+    const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(id) },
+    });
+
+    if (!issue) {
+        return NextResponse.json({ message: "Invalid Issue" }, { status: 404 });
+    }
+
+    await prisma.issue.delete({ where: { id: parseInt(id) } });
+    return NextResponse.json({});
+}
