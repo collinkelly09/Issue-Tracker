5547ca437965a900e8aeb104a2bc3e0d9c67e6c7
Collin Kelly <collinkelly09@gmail.com>
2024-11-22 12:06:26 -0700

    create registration and credentials login

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -8,3 +8,17 @@ export const issueSchema = z.object({
 export const issueStatusSchema = z.object({
     status: z.enum(["OPEN", "IN_PROGRESS", "CLOSED"]),
 });
+
+export const userSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z
+        .string()
+        .min(8, "Password must be at least 8 characters")
+        .max(30, "Password cannot exceed 30 characters")
+        .regex(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+            "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character"
+        ),
+    confirmPassword: z.string().min(8).max(30),
+});
