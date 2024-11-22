bf2b53b49ed9d4b40368a293ac1cbeb812dc1b41
Collin Kelly <collinkelly09@gmail.com>
2024-11-21 21:07:15 -0700

    add delete button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "@radix-ui/themes/styles.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Container, Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
 // const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -22,7 +22,9 @@ export default function RootLayout({
             <body>
                 <Theme accentColor="iris" radius="large">
                     <NavBar />
-                    <main className="p-5">{children}</main>
+                    <main className="p-5">
+                        <Container>{children}</Container>
+                    </main>
                 </Theme>
             </body>
         </html>
