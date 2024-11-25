45382039174a0871554cdb83f9479852888c197a
Collin Kelly <collinkelly09@gmail.com>
2024-11-24 17:23:09 -0700

    protect routes

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -2,13 +2,19 @@ import { Issue } from "@prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import ReactMarkdown from "react-markdown";
 import IssueStatusDropdown from "../_components/IssueStatusDropdown";
+import authOptions from "@/app/api/auth/authOptions";
+import { getServerSession } from "next-auth";
+import { IssueStatusBadge } from "@/app/components";
 
-const IssueDetails = ({ issue }: { issue: Issue }) => {
+const IssueDetails = async ({ issue }: { issue: Issue }) => {
+    const session = await getServerSession(authOptions);
+
     return (
         <>
             <Heading>{issue.title}</Heading>
             <Flex gap="3" my="2">
-                <IssueStatusDropdown issue={issue} />
+                {session && <IssueStatusDropdown issue={issue} />}
+                {!session && <IssueStatusBadge status={issue.status} />}
                 <Text>{issue.createdAt.toDateString()}</Text>
             </Flex>
             <Card className="prose max-w-full" mt="4">
