a9def8a5f3823911181fc69fe5cd9ed77ba36461
Collin Kelly <collinkelly09@gmail.com>
2024-12-03 16:17:31 -0700

    optimize performance using react cache

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import LatestIssues from "./LatestIssues";
 import IssueChart from "./IssueChart";
 import { Flex, Grid } from "@radix-ui/themes";
 import { Status } from "@prisma/client";
+import { Metadata } from "next";
 
 export default async function Home() {
     const open = await prisma.issue.count({ where: { status: "OPEN" } });
@@ -34,3 +35,8 @@ export default async function Home() {
         </Grid>
     );
 }
+
+export const metadata: Metadata = {
+    title: "Issue Tracker - Dashboard",
+    description: "View a summary of project issues",
+};
